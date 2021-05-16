5e92cc0c10376fa1e3095357c3e8e26ef14a4f3f
walkonprogrammer <underdogg83@icloud.com>
2021-05-15 22:16:51 -0700

    added code for players turn

---
+++ rock_paper_scissors.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+# contains the code to import the function used in this project.
+# randit is used to generate random numbers
+from random import randint
+
+# allow the player to choose Rock, Paper, Scissors by typing the letter 'r','p', or 's'
+player = input('rock (r), paper(p) or scissors(s)?')
+
+print(player, 'vs')
